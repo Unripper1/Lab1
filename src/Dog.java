a3f7ce4671af8ebd26b9058a734fc9dc98d1b655
Unripper1 <dmharitoh@gmail.com>
2020-09-11 10:05:02 +0300

    In process

---
+++ src/Dog.java
@@ -0,0 +1,35 @@
+public abstract class Dog {
+    protected String name;
+    public void setName(String name){
+        this.name=name;
+    };
+    public abstract void showName();
+}
+class Bulldog extends Dog{
+    public void showName() {
+        System.out.println(name);
+    }
+
+}
+class Husky extends Dog{
+    public void showName() {
+        System.out.println(name);
+    }
+
+
+}
+
+class Pug extends Dog{
+    public void showName() {
+        System.out.println(name);
+    }
+
+
+}
+class Main3 {
+    public static void main(String[] args){
+        Dog husky = new Husky();
+        husky.setName("SK");
+        husky.showName();
+    }
+}
